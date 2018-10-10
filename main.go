0cba9537c0d855836d44856495a5a5138a68b590
Vignon Hounwanou <vignon.hounwanou@gmail.com>
2018-10-10 15:42:26 +0200

    Going to push to Heroku and Git

--- main.go
+++ main.go
@@ -2,11 +2,12 @@
 package main
 
 import (
- 	"log"
- 	"net/http"
- 	"os"
+	"log"
+	"net/http"
+	"os"
+	"sigfox-api/store"
+
 	"github.com/gorilla/handlers"
-	"rest-and-go/store"
 )
 
 func main() {
@@ -19,9 +20,9 @@ func main() {
 	router := store.NewRouter() // create routes
 
 	// These two lines are important in order to allow access from the front-end side to the methods
-	allowedOrigins := handlers.AllowedOrigins([]string{"*"}) 
- 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
 
 	// Launch server with CORS validations
- 	log.Fatal(http.ListenAndServe(":" + port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
 }
